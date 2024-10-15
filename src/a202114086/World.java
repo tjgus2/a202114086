38acf3c5b8a6c3f48f888b4fe917c7844541d666
Park, Seohyun <tjgus6279@naver.com>
2024-10-15 22:12:44 +0900

    fourth

--- src/a202114086/World.java
+++ src/a202114086/World.java
@@ -2,8 +2,10 @@ package a202114086;
 
 public class World {
 
-	  public static void run() {
-	    System.out.println("안녕하세요");
-	  }
-
+	public static void run() {
+	  System.out.println("안녕하세요");
+	  System.out.println("박서현");
 	}
+  
+  }
+  
